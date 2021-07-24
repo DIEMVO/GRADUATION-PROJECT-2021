b21b61a74e9ebffb302132eab56d6bace3a33ae8
Gianhung2912 <43291586+Gianhung2912@users.noreply.github.com>
2021-07-24 19:12:30 +0700

    update fix test1

--- Autotest/WebTestApp/WebApp/BlazorApp1/Models/Project.cs
+++ Autotest/WebTestApp/WebApp/BlazorApp1/Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace BlazorApp1.Models
 {
     public class Project : BaseModel
     {
+        [Required]
         public string ProjectName { get; set; }
 
+        [Required]
         public string Link { get; set; }
 
         public virtual ICollection<ProjectDetail> ProjectDetails { get; set; }
