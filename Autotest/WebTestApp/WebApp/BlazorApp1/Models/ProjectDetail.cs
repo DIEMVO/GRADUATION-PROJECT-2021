ec1ffef4b7d3cdeec38e753902433640f5468b95
Gianhung2912 <43291586+Gianhung2912@users.noreply.github.com>
2021-07-26 19:39:35 +0700

    update

--- Autotest/WebTestApp/WebApp/BlazorApp1/Models/ProjectDetail.cs
+++ Autotest/WebTestApp/WebApp/BlazorApp1/Models/ProjectDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace BlazorApp1.Models
 {
     public class ProjectDetail : BaseModel
     {
+        [Required]
         public string FunctionName { get; set; }
 
         public string Milestones { get; set; }
